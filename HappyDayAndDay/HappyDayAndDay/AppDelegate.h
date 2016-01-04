5510950eb9a468035bad76fe6f749399c63e4e0c
张衡 <838579111@qq.com>
2016-01-04 13:36:37 +0800

    First

---
+++ HappyDayAndDay/HappyDayAndDay/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HappyDayAndDay
+//
+//  Created by scjy on 16/1/4.
+//  Copyright © 2016年 张衡. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
